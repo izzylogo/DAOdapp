f1fb265e105d0b4b13d519cfba682992a944ce93
Israel Omitiran <omitiranisrael@gmail.com>
2022-06-05 14:38:10 +0100

    Done

---
+++ hardhat-tutorial/constants.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0x4B64EbB2C6d5C5e6f62cA7dD613CE9C53f2C9175";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
